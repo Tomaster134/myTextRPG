4b7bd1e47f681e1d4199a34058f7f893a95e88fc
Tomaster134 <tomaster134@gmail.com>
2024-02-26 20:53:52 -0500

    maybe I got it?

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from app import create_app, socketio
-from gevent.pywsgi import WSGIServer
 
 app = create_app()
 
--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask
 from flask_socketio import SocketIO
 from config import Config
